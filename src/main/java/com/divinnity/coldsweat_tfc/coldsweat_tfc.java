d228d121d64a088560d959ed32375a799b19dc80
Sebastien Bruckert <seb.bruckert@gmail.com>
2024-08-01 15:57:45 +0200

    Lot of changes, the biggest one is that now, it is called ColdSweat_TFC, and the TempModifierPlacement has changed. Also, it removes the default BiomeTemperature.

--- src/main/java/com/divinnity/coldsweat_tfc/coldsweat_tfc.java
+++ src/main/java/com/divinnity/coldsweat_tfc/coldsweat_tfc.java
@@ -1,26 +1,21 @@
-package com.divinnity.hardrock;
+package com.divinnity.coldsweat_tfc;
 
 import org.slf4j.Logger;
+
 import com.mojang.logging.LogUtils;
+
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(Hardrock.MOD_ID)
-public class Hardrock
+@Mod(coldsweat_tfc.MOD_ID)
+public class coldsweat_tfc
 {
     public static final Logger LOGGER = LogUtils.getLogger();
-    public static final String MOD_ID = "hardrock";
+    public static final String MOD_ID = "coldsweat_tfc";
 
-    public Hardrock()
+    public coldsweat_tfc()
     {
         MinecraftForge.EVENT_BUS.register(this);
-
-        if (ModList.get().isLoaded("cold_sweat")) {
-            LOGGER.info("ColdSweat detected and loaded.");
-        }
     }
-
-
 }
---
+++ src/main/java/com/divinnity/coldsweat_tfc/common/Events.java
@@ -0,0 +1,33 @@
+package com.divinnity.hardrock.common;
+
+import com.divinnity.hardrock.compat.ColdSweatTempModifier;
+import com.momosoftworks.coldsweat.api.event.core.TempModifierRegisterEvent;
+import com.momosoftworks.coldsweat.api.temperature.modifier.TempModifier;
+import com.momosoftworks.coldsweat.api.util.Temperature;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import com.momosoftworks.coldsweat.api.temperature.modifier.BiomeTempModifier;
+
+@Mod.EventBusSubscriber
+public class Events
+{
+    public final static TempModifier ColdSweatModifierCompatibility = new ColdSweatTempModifier().tickRate(5);
+
+    @SubscribeEvent
+    public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+        Player player = event.getPlayer();
+        if (!player.level.isClientSide) {
+            Temperature.removeModifiers(player, Temperature.Type.WORLD, mod -> mod instanceof BiomeTempModifier);
+            Temperature.addModifier(player, ColdSweatModifierCompatibility, Temperature.Type.WORLD, false, com.momosoftworks.coldsweat.api.util.Temperature.Addition.BEFORE_FIRST);
+        }
+    }
+
+    @SubscribeEvent
+    public static void registerTempModifiers(TempModifierRegisterEvent event)
+    {
+        event.registerByClassName("com.divinnity.hardrock.compat.ColdSweatTempModifier");
+    }
+}
